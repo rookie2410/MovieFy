6825868cde9c029ebb615d1a9760f5d69340ce38
Roshan Gupta <43931122+rookie2410@users.noreply.github.com>
2019-08-14 11:14:33 +0530

    Finally adding the Procfile

--- OMDB.py
+++ OMDB.py
@@ -3,7 +3,7 @@ import json
 
 def getRating(movieTitle):
     url = 'http://www.omdbapi.com'
-    data = {'apikey':'Enter the API here','t':movieTitle}
+    data = {'apikey':'34af84e4','t':movieTitle}
     response = requests.get(url,data)
     return str(response.json().get("imdbRating"))
     
