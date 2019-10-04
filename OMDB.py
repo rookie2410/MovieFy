bc834333d18171dd1cd5daa3ce8eb1ce4881ce04
rookie2410 <43931122+rookie2410@users.noreply.github.com>
2019-10-04 23:00:57 +0530

    Push on Heroku

--- OMDB.py
+++ OMDB.py
@@ -4,7 +4,7 @@ import requests
 def get_movie_info(movieTitle):
     url = 'http://www.omdbapi.com'
     #api_key = os.getenv('APIKEY')
-    api_key = 34af84e4
+    api_key = "34af84e4"
     data = {'apikey':api_key,'t':movieTitle}
     response = requests.get(url,data).json()
 
